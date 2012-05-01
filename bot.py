696f95925d4c9cf5b74e55d79a74400f5e1d3cc5
adarsha joisa <adarshajoisa@gmail.com>
2012-05-01 18:07:45 +0530

    reorganized code to retain context information

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
+# imports
 import re
+import predefined_responses
 import weather
 import sentence
 import readfile
@@ -6,56 +8,83 @@ import temperature
 import string
 from string import lower
 
-def bot():
-  conversation = []
-  location = ''
-  time = 'today'
-  key = ''
-  keytemplate = []
-  fulltime = ''
-  numdays = ''
+# keyword conditions
+condnext = False
+condweather = False
+condtime = False
+condlocation = False
+condtemp = False
+condkey = False
 
-  citylist = readfile.readfile('cities.txt')
-  keylist = readfile.readfile('keywords.txt')
-  timelist = readfile.readfile('time.txt')
-  condlist = readfile.readfile('conditions.txt')
-  numlist = readfile.readfile('numbers.txt')
-  exitlist = ['exit', 'quit', 'bye', 'ok']
+# global variables
+conversation = []
+location = ''
+prevlocation = location 
+time = 'today'
+key = ''
+keytemplate = []
+fulltime = ''
+numdays = ''
 
-  print 'Hello! You can ask me questions about the weather in any major city in the world. What would you like to know?'
-  while True :
-    input = raw_input('Me > ')
-    if input in exitlist:
-      break
+
+# read data files
+citylist = readfile.readfile('cities.txt')
+keylist = readfile.readfile('keywords.txt')
+timelist = readfile.readfile('time.txt')
+condlist = readfile.readfile('conditions.txt')
+numlist = readfile.readfile('numbers.txt')
+exitlist = ['exit', 'quit', 'bye', 'ok']
+
+# Greeting message
+print 'Hello! You can ask me questions about the weather in any major city in the world. What would you like to know?'
+
+# Start main loop
+while True :
+  
+  # read input from user
+  input = raw_input('Me > ')
+  if input in exitlist:
+    break
     
-    currentstring = input.split()
-    conversation.append(currentstring)
-    
-    if 'next' in currentstring:
-      numdays = currentstring[currentstring.index('next') + 1]
-      for i in numlist:
-	if numdays == i[0]:
-	  numdays = i[1]
-	  break
-      if re.match('[0-9]*$',numdays):
-	numdays = int(numdays)
-      else:
-	numdays = ''
-    
-    if 'weather' in currentstring:
-      key = ''
-      keytemplate = []
-    # get key from input
-    for i in keylist:
-      if i[0] in input:
-	key = i[0]
-	keytemplate = i
+  currentstring = input.split()
+  conversation.append(currentstring)
+  
+  # Start searching input for each of the keywords
+
+  if 'next' in currentstring:
+    condnext = True
+    condtime = False
+    numdays = currentstring[currentstring.index('next') + 1]
+    for i in numlist:
+      if numdays == i[0]:
+	numdays = i[1]
 	break
-    
-    # get time from input
+    if re.match('[0-9]*$',numdays):
+      numdays = int(numdays)
+    else:
+      numdays = ''
+  
+  if 'weather' in currentstring:
+    condweather = True
+    condkey = False
+    condtemp = False
+    key = ''
+    keytemplate = []
 
+  # get key from input
+  for i in keylist:
+    if i[0] in input:
+      condkey = True
+      condweather = False
+      condtemp = False
+      key = i[0]
+      keytemplate = i
+      break
+
+  # get time from input
     for i in timelist:
       if lower(i[0]) in input:
+	condtime = True
 	numdays = ''
 	if lower(i[0]) != 'today' and lower(i[0]) != 'tomorrow':
 	  time = i[1]
@@ -65,135 +94,149 @@ def bot():
 	  time = i[0]
 	  fulltime = time
 	  break
-    if fulltime == '':
-      fulltime = time
+  if fulltime == '':
+    fulltime = time
 
-    if numdays != '':
-      if numdays > 4:
-	print 'Forecast is available only for the next 4 days.'
-      else:
-	time = ''
-	fulltime = ''
-	count = numdays
-    prevlocation = location 
-    #We store previous location to avoid re-fetching data if the location hasn't been changed
-    
-    
-    # Below, we check if any token in the input matches a city name, and if so, set location to that city
-    newlocation = False
-    
+  if numdays != '':
+    condtime = True
+    if numdays > 4:
+      print 'Forecast is available only for the next 4 days.'
+    else:
+      time = ''
+      fulltime = ''
+      count = numdays
+  
     # get location from input
-    foundLocation = False
-    for i in citylist:
-      if lower(i[0]) in input:
-	location = i[0]
-	foundLocation = True
-	break
+  foundLocation = False
+  for i in citylist:
+    if lower(i[0]) in input:
+      condlocation = True
+      location = i[0]
+      foundLocation = True
+      break
+  
+  # find if a new location has been mentioned. if not, don't fetch data again
+  if location is not prevlocation:
+    newlocation = True
+  
+  if location is '':
+    if prevlocation is '':
+      print 'City not found'
+    else:
+      location = prevlocation
+      newlocation = False
+  
+  location = location.replace(' ','-') #Google requires a '-' in 2-word city names
+  result = False
+  
+  # get temperature from input
+  if 'temperature' in currentstring:
+    condtemp = True
+  
+  if not( condtemp or condlocation or condkey or condlist or condnext or condtime or condweather ):
+    predefined_responses.respond()
+  
+  if location is not '':
+    if newlocation:	#If location hasn't changed, don't fetch data again. It's already available
+      print 'Fetching weather information from Google...'
+      # Call Google weather to get current weather conditions
+      google_result = weather.get_weather(location)
     
-    if not foundLocation:
-      if location != '':
-	print "I didn't find any city name in your input. I'll get you information about " + location
-    # find if a new location has been mentioned. if not, don't fetch data again
-    if location is not prevlocation:
-      newlocation = True
     
-    if location is '':
-      if prevlocation is '':
-	print 'City not found'
-      else:
-	location = prevlocation
-	newlocation = False
+# We have a valid location. Get further information
+
+# User has asked about temperature. Return temperature information and continue
+    if condtemp:
+      print temperature.temperature(google_result, time)
+      continue
     
-    location = location.replace(' ','-') #Google requires a '-' in 2-word city names
-    result = False
-    
-    
-    if location is not '':
-      if newlocation:	#If location hasn't changed, don't fetch data again. It's already available
-	print 'Fetching weather information from Google...'
-	# Call Google weather to get current weather conditions
-	google_result = weather.get_weather(location)
+# User has asked about a specific weather condition. Print information. There are 2 possibilities:
+#    1. Find the condition in the next n days
+#    2. Find the condition in a specified day
+
+    if condkey:
       
-      if 'temperature' in currentstring:
-	print temperature.temperature(google_result, time)
+# 1. User has asked about a specific condition in the 'next x days'. Return appropriate response
+      printstring = ''
+      timecounter = 0
+      
+      day_of_week = ''
+      condition = ''
+      if numdays != '':
+	for i in google_result['forecasts']:
+	  count -= 1
+	  if count < 0:
+	    break
+	  if key in lower(i['condition']):
+	    result = True
+	    day_of_week = i['day_of_week']
+	    condition = i['condition']
+	    break
+	
+	for i in timelist:
+	  if i[0] != 'today' and i[0] != 'tomorrow':
+	    if i[1] == day_of_week:
+	      fulltime = i[0]
+	      break
+	if result:
+	  printstring = keytemplate[3] + keytemplate[0] + ' on ' + fulltime
+	else:
+	  printstring = keytemplate[4] + keytemplate[0] + ' in the next ' + str(numdays) + ' days.'
+	
+	print printstring
 	continue
       
-      printed = False
-      
-      
-      if key is not '':
-	printstring = ''
-	timecounter = 0
-	
-	day_of_week = ''
-	condition = ''
-	if numdays != '':
-	  for i in google_result['forecasts']:
-	    count -= 1
-	    if count < 0:
-	      break
+# 2. User has asked about a particular condition on a particular day. Return appropriate response
+      if time != 'today' and time != 'tomorrow':
+	for i in google_result['forecasts']:
+	  if i['day_of_week'] == time:
 	    if key in lower(i['condition']):
-	      result = True
-	      day_of_week = i['day_of_week']
-	      condition = i['condition']
-	      break
-	  
-	  for i in timelist:
-	    if i[0] != 'today' and i[0] != 'tomorrow':
-	      if i[1] == day_of_week:
-		fulltime = i[0]
-		break
-	  if result:
-	    printstring = keytemplate[3] + keytemplate[0] + ' on ' + fulltime
-	  else:
-	    printstring = keytemplate[4] + keytemplate[0] + ' in the next ' + str(numdays) + ' days.'
-	  
-	  print printstring
-	  printed = True
-	      
-	if not printed:
-	  if time != 'today' and time != 'tomorrow':
-	    for i in google_result['forecasts']:
-	      if i['day_of_week'] == time:
-		if key in lower(i['condition']):
-		  printstring = keytemplate[3] + keytemplate[0] + ' on'
-		else:
-		  printstring = keytemplate[4] + keytemplate[0] + ' on'
-	  elif time == 'today':
-	    fulltime = time
-	    if key in lower(google_result['current_conditions']['condition']):
-	      printstring = keytemplate[1] + keytemplate[0]
+	      printstring = keytemplate[3] + keytemplate[0] + ' on'
 	    else:
-	      printstring = keytemplate[2] + keytemplate[0]
-	  elif time == 'tomorrow':
-	    fulltime = time
-	    if key in lower(google_result['forecasts'][1]['condition']):
-	      printstring = keytemplate[3] + keytemplate[0]
-	    else:
-	      printstring = keytemplate[4] + keytemplate[0]
-		
-	  print printstring, fulltime
-
-      elif time == '' or time == 'today' :
-	  printstring = sentence.sentence(google_result['current_conditions']['condition'], time)
-	  print printstring, fulltime,  google_result['current_conditions']['humidity'], google_result['current_conditions']['wind_condition']
-      else :
-	if time == 'tomorrow':
-	  printstring = sentence.sentence(google_result['forecasts'][1]['condition'], time)
-	  print printstring, fulltime
+	      printstring = keytemplate[4] + keytemplate[0] + ' on'
+      elif time == 'today':
+	fulltime = time
+	if key in lower(google_result['current_conditions']['condition']):
+	  printstring = keytemplate[1] + keytemplate[0]
 	else:
-	  found = False
-	  for i in range(4):
-	    if google_result['forecasts'][i]['day_of_week'] == time:
-	      printstring = sentence.sentence(google_result['forecasts'][i]['condition'], time)
-	      print printstring, "on", fulltime
-	      found = True
-	  if not found:
-	    print "Forecast for " + time + " is not available currently."
-
+	  printstring = keytemplate[2] + keytemplate[0]
+      elif time == 'tomorrow':
+	fulltime = time
+	if key in lower(google_result['forecasts'][1]['condition']):
+	  printstring = keytemplate[3] + keytemplate[0]
+	else:
+	  printstring = keytemplate[4] + keytemplate[0]
+	      
+      print printstring, fulltime
+      printed = True
+      continue
+      
+# User is asking about today's weather. Print details
+    elif time == '' or time == 'today' :
+      printstring = sentence.sentence(google_result['current_conditions']['condition'], time)
+      print printstring, fulltime,  google_result['current_conditions']['humidity'], google_result['current_conditions']['wind_condition']
+      continue
+      
+# User is asking about weather of a particular day. Print details
+    else :
+      if time == 'tomorrow':
+	printstring = sentence.sentence(google_result['forecasts'][1]['condition'], time)
+	print printstring, fulltime
+      else:
+	found = False
+	for i in range(4):
+	  if google_result['forecasts'][i]['day_of_week'] == time:
+	    printstring = sentence.sentence(google_result['forecasts'][i]['condition'], time)
+	    print printstring, "on", fulltime
+	    found = True
+	if not found:
+	  print "Forecast for " + time + " is not available currently."
+      continue
     
-    else:
-      print 'What\'s the location?'
-  #end of outermost while loop
-  print 'ending the program...'
-  print 'bye!'
+  else:
+    print 'What\'s the location?'
+# End of outermost while loop.
+
+# Print message before exiting program
+print 'ending the program...'
+print 'bye!'
