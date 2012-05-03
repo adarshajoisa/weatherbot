885ca9e5eaf77adeb51023d2d3bab8e830fe4627
adarsha joisa <adarshajoisa@gmail.com>
2012-05-03 23:48:24 +0530

    added logging and country info

--- bot.py
+++ bot.py
@@ -14,6 +14,7 @@ import weather
 import sentence
 import readfile
 import temperature
+import log
 import string
 from string import lower
 
@@ -28,6 +29,7 @@ def chat():
   condresponse = False
   foundinfo = False
   condtrain = False
+  condcountry = False
 
   # global variables
   conversation = []
@@ -38,6 +40,8 @@ def chat():
   keytemplate = []
   fulltime = ''
   numdays = ''
+  logstr = ''
+  printstr = ''
   responsedict = {} 	# Dictionary to hold all inputs without predefined responses. This dictionary will be written into predefined_responses.txt before exiting the program.
 
 
@@ -47,17 +51,22 @@ def chat():
   timelist = readfile.readfile('time.txt')
   condlist = readfile.readfile('conditions.txt')
   numlist = readfile.readfile('numbers.txt')
+  countrylist = readfile.readfile('countries.txt')
   exitlist = ['exit', 'quit', 'bye', 'ok']
 
   # Greeting message
-  print 'Hello! You can ask me questions about the weather in any major city in the world. What would you like to know?'
+  printstr =  'Hello! You can ask me questions about the weather in any major city in the world. What would you like to know?'
+  print printstr
+  logstr += '\n\n' + printstr
 
   # Start main loop
   while True :
     foundinfo = False
     condtrain = False
+    condcountry = False
     # read input from user
-    input = raw_input('Me > ')
+    input = raw_input('\nMe > ')
+    logstr += '\nMe > ' + input + '\nBot > '
     if input in exitlist:
       if input == 'ok':
 	exitans = raw_input("Do you want to quit? (y/n)")
@@ -74,22 +83,40 @@ def chat():
     
     if input == 'train':
       condtrain = True
-      print 'Entering training mode. Enter input and response seperated by a "/": input/response. Type "exit" to quit training mode'
+      printstr =  'Entering training mode. Enter input and response seperated by a "|": input|response. Type "exit" to quit training mode'
+      print printstr
+      logstr += '\n' + printstr + '\n'
+      
       while True:
 	traininput = raw_input('>')
 	if traininput == 'exit':
 	  break
-	if traininput.find('/') < 0:
-	  print 'Format error: use input/response'
+	if traininput.find('|') < 0:
+	  printstr =  'Format error: use input|response'
+	  print printstr
+	  logstr += '\n' + printstr + '\n'
 	  continue
-	traininput = traininput.split('/')
+	traininput = traininput.split('|')
 	responsedict[traininput[0]] = traininput[1]
     
     if condtrain:
       continue
     
 
-    if 'next' in currentstring:
+    for i in countrylist:
+      if i[0] in currentstring:
+	printstr = 'Which city in ' + i[0] + '?'
+	condcountry = True
+	foundinfo = True
+	break
+      
+    if condcountry:
+      print printstr
+      logstr += printstr
+      continue
+    
+
+    if 'next' in input:
       foundinfo = True
       condnext = True
       condtime = False
@@ -103,7 +130,7 @@ def chat():
       else:
 	numdays = ''
     
-    if 'weather' in currentstring:
+    if 'weather' in input:
       foundinfo = True
       condweather = True
       condkey = False
@@ -114,13 +141,16 @@ def chat():
     # get key from input
     for i in keylist:
       if i[0] in input:
-	foundinfo = True
-	condkey = True
-	condweather = False
-	condtemp = False
-	key = i[0]
-	keytemplate = i
-	break
+	if 'sunday' in lower(input) and i[0] == 'sun':
+	  break
+	else:
+	  foundinfo = True
+	  condkey = True
+	  condweather = False
+	  condtemp = False
+	  key = i[0]
+	  keytemplate = i
+	  break
 
     # get time from input
       for i in timelist:
@@ -142,7 +172,9 @@ def chat():
     if numdays != '':
       condtime = True
       if numdays > 4:
-	print 'Forecast is available only for the next 4 days.'
+	printstr =  'Forecast is available only for the next 4 days.'
+	print printstr
+	logstr += '\n' + printstr + '\n'
       else:
 	time = ''
 	fulltime = ''
@@ -175,7 +207,7 @@ def chat():
     result = False
     
     # get temperature from input
-    if 'temperature' in currentstring:
+    if 'temperature' in input:
       foundinfo = True
       condtemp = True
 
@@ -183,16 +215,23 @@ def chat():
     if not foundinfo:
       response = predefined_responses.respond(input, responsedict)
       if response == '':
-	print "I don't know what that means. If I asked you the same question, what would you reply?"
+	printstr =  "I don't know what that means. If I asked you the same question, what would you reply?"
+	print printstr
+	logstr += printstr
 	responseinput = raw_input('Me > ')
+	logstr += '\nMe > ' + responseinput
 	responsedict[input] = responseinput
       else:
-	print response
+	printstr =  response
+	print printstr
+	logstr += printstr
       continue
     
     if condlocation:
       if newlocation:	#If location hasn't changed, don't fetch data again. It's already available
-	print 'Fetching weather information from Google...'
+	printstr =  'Fetching weather information from Google...'
+	print printstr
+	logstr += printstr
 	# Call Google weather to get current weather conditions
 	google_result = weather.get_weather(location)
       
@@ -201,7 +240,9 @@ def chat():
 
   # User has asked about temperature. Return temperature information and continue
       if condtemp:
-	print temperature.temperature(google_result, time)
+	printstr =  temperature.temperature(google_result, time)
+	print printstr
+	logstr += printstr
 	continue
       
   # User has asked about a specific weather condition. Print information. There are 2 possibilities:
@@ -209,11 +250,11 @@ def chat():
   #    2. Find the condition in a specified day
 
       if condkey:
-	
+
   # 1. User has asked about a specific condition in the 'next x days'. Return appropriate response
-	printstring = ''
+	printstr = ''
 	timecounter = 0
-	
+
 	day_of_week = ''
 	condition = ''
 	if numdays != '':
@@ -226,69 +267,82 @@ def chat():
 	      day_of_week = i['day_of_week']
 	      condition = i['condition']
 	      break
-	  
+
 	  for i in timelist:
 	    if i[0] != 'today' and i[0] != 'tomorrow':
 	      if i[1] == day_of_week:
 		fulltime = i[0]
 		break
 	  if result:
-	    printstring = keytemplate[3] + keytemplate[0] + ' on ' + fulltime
+	    printstr = keytemplate[3] + keytemplate[0] + ' on ' + fulltime
 	  else:
-	    printstring = keytemplate[4] + keytemplate[0] + ' in the next ' + str(numdays) + ' days.'
-	  
-	  print printstring
+	    printstr = keytemplate[4] + keytemplate[0] + ' in the next ' + str(numdays) + ' days.'
+
+	  print printstr
+	  logstr += printstr
 	  continue
-	
+
   # 2. User has asked about a particular condition on a particular day. Return appropriate response
 	if time != 'today' and time != 'tomorrow':
 	  for i in google_result['forecasts']:
 	    if i['day_of_week'] == time:
 	      if key in lower(i['condition']):
-		printstring = keytemplate[3] + keytemplate[0] + ' on'
+		printstr = keytemplate[3] + keytemplate[0] + ' on'
 	      else:
-		printstring = keytemplate[4] + keytemplate[0] + ' on'
+		printstr = keytemplate[4] + keytemplate[0] + ' on'
 	elif time == 'today':
 	  fulltime = time
 	  if key in lower(google_result['current_conditions']['condition']):
-	    printstring = keytemplate[1] + keytemplate[0]
+	    printstr = keytemplate[1] + keytemplate[0]
 	  else:
-	    printstring = keytemplate[2] + keytemplate[0]
+	    printstr = keytemplate[2] + keytemplate[0]
 	elif time == 'tomorrow':
 	  fulltime = time
 	  if key in lower(google_result['forecasts'][1]['condition']):
-	    printstring = keytemplate[3] + keytemplate[0]
+	    printstr = keytemplate[3] + keytemplate[0]
 	  else:
-	    printstring = keytemplate[4] + keytemplate[0]
-		
-	print printstring, fulltime
-	printed = True
+	    printstr = keytemplate[4] + keytemplate[0]
+
+	printstr =  printstr + ' ' + fulltime
+	print printstr
+	logstr += printstr
 	continue
-	
+
   # User is asking about today's weather. Print details
       elif time == '' or time == 'today' :
-	printstring = sentence.sentence(google_result['current_conditions']['condition'], time)
-	print printstring, fulltime,  google_result['current_conditions']['humidity'], google_result['current_conditions']['wind_condition']
+	printstr = sentence.sentence(google_result['current_conditions']['condition'], time)
+	printstr += ' ' + fulltime + '. ' + google_result['current_conditions']['humidity'] + ' '
+	if google_result['current_conditions'].has_key('wind_condition'):
+	  printstr += google_result['current_conditions']['wind_condition']
+	print printstr
+	logstr += printstr
 	continue
-	
+
   # User is asking about weather of a particular day. Print details
-      else :
-	if time == 'tomorrow':
-	  printstring = sentence.sentence(google_result['forecasts'][1]['condition'], time)
-	  print printstring, fulltime
-	else:
-	  found = False
-	  for i in range(4):
-	    if google_result['forecasts'][i]['day_of_week'] == time:
-	      printstring = sentence.sentence(google_result['forecasts'][i]['condition'], time)
-	      print printstring, "on", fulltime
-	      found = True
-	  if not found:
-	    print "Forecast for " + time + " is not available currently."
+      elif time == 'tomorrow':
+	printstr = sentence.sentence(google_result['forecasts'][1]['condition'], time)
+	printstr += ' ' + fulltime
+	print printstr
+	logstr += printstr
+      else:
+	found = False
+	for i in range(4):
+	  if google_result['forecasts'][i]['day_of_week'] == time:
+	    printstr = sentence.sentence(google_result['forecasts'][i]['condition'], time)
+	    printstr +=   " on" + ' ' +  fulltime
+	    print printstr
+	    logstr += printstr
+	    found = True
+	if not found:
+	  printstr =  "Forecast for " + time + " is not available currently."
+	  print printstr
+	  logstr += printstr
 	continue
       
     else:
-      print 'What\'s the location?'
+      printstr =  'What\'s the location?'
+      print printstr
+      logstr += printstr
   # End of outermost while loop.
 
   # Print message before exiting program
@@ -296,12 +350,15 @@ def chat():
   for i in responsedict:
     dictcount += 1
   if dictcount > 0:
-    print 'Writing new entries to database...'
+    printstr =  'Writing new entries to database...'
+    print printstr
+    logstr += printstr
   datafile = file('predefined_responses.txt', 'a')
   for i in responsedict.keys():
     trimmedi = re.sub('[^a-zA-Z0-9 ]+','', i)
-    string = trimmedi + '/' + responsedict[i] + '\n'
+    string = trimmedi + '|' + responsedict[i] + '\n'
     datafile.write(string)
+  log.log(logstr)
   print 'Ending the program...'
   print 'Bye!'
   
