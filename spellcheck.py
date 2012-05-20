11349328707f10732e9c1b3b338a6ce6d2c4ef27
adarsha joisa <adarshajoisa@gmail.com>
2012-05-20 21:11:23 +0530

    edit indentation

--- spellcheck.py
+++ spellcheck.py
@@ -8,15 +8,14 @@ def train(features):
     model[f] += 1
   return model
 
-NWORDS = train(words(file('plaintext').read()))
+NWORDS = train(words(file('big.txt').read()))
 
 def edits1(word):
   n = len(word)
   return set([word[0:i]+word[i+1:] for i in range(n)] + ## deletion
-
-[word[0:i]+word[i+1]+word[i]+word[i+2:] for i in range(n-1)] + ## transposition
-[word[0:i]+c+word[i+1:] for i in range(n) for c in string.lowercase] + ## alteration
-[word[0:i]+c+word[i:] for i in range(n+1) for c in string.lowercase]) ## insertion
+  [word[0:i]+word[i+1]+word[i]+word[i+2:] for i in range(n-1)] + ## transposition
+  [word[0:i]+c+word[i+1:] for i in range(n) for c in string.lowercase] + ## alteration
+  [word[0:i]+c+word[i:] for i in range(n+1) for c in string.lowercase]) ## insertion
 
 def known_edits2(word):
   return set(e2 for e1 in edits1(word) for e2 in edits1(e1) if e2 in NWORDS)
